c4ac151bf6f99b190a87a46fb05d207c392448ff
soumahuka0622-hub <souma.huka0622@gmail.com>
2026-03-02 22:39:13 +0900

    feat: add favicon icon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import './globals.css';
 export const metadata = {
   title: 'Butterfly Tea X Client',
   description: 'No timeline X client prototype',
+  icons: {
+    icon: '/icon.svg',
+  },
 };
 
 export default function RootLayout({ children }) {
